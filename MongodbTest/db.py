04133c6bc7d5d569a83c05e5b5e7bed663f8ce71
GwanWooPark <pgw4712@icloud.com>
2021-06-08 15:34:27 +0900

    Python

--- MongodbTest/db.py
+++ MongodbTest/db.py
@@ -4,7 +4,9 @@ db = client.newDB  # db 를 하나 만들고
 userCollection = db.user  # db 에서 collection 을 만든다.
 
 # insert_one() 으로 db 를 추가
-userCollection.insert_one({'name': 'mik_a', 'age': '100', })
+userCollection.insert_one({'name': 'mik_a', 'age': '100'})
+userCollection.insert_one({'name': 'kim', 'age': '100'})
+userCollection.insert_one({'name': 'park', 'age': '100'})
 
 # find() 로 collection 에 저장된 데이터를 불러와서 출력한다
 for i in userCollection.find():
