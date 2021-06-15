8db4e319f9e4314b31d1315d6db12782e855d5b7
GwanWooPark <pgw4712@icloud.com>
2021-06-15 16:12:42 +0900

    Python

--- hello/hello02/urls.py
+++ hello/hello02/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.hello),
     path('var01', views.var01),
     path('var02', views.var02),
+    path('for', views.forLoop),
+    path('if01', views.if01),
+    path('if02', views.if02),
+    path('href', views.href)
 ]
--- hello/hello02/views.py
+++ hello/hello02/views.py
@@ -10,6 +10,22 @@ def var01(request):
     lst = ['Python', 'Django', 'Template']
     return render(request, 'variable01.html', {'lst': lst})
 
+
 def var02(request):
     dct = {'class': 'qclass', 'name': '관우'}
     return render(request, 'variable02.html', {'dct': dct})
+
+
+def forLoop(request):
+    return render(request, 'for.html', {'numbers': range(1, 10)})
+
+
+def if01(request):
+    return render(request, 'if01.html', {'user': {'id': 'pgw4712', 'name': '박관우'}})
+
+
+def if02(request):
+    return render(request, 'if02.html', {'role': 'admin'})
+
+def href(request):
+    return render(request, 'href.html')
